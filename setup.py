2f9318b21fa9797e4ed8e24e6f4eb8bf0fa85c11
CurdinLieberherr <81492585+CurdinLieberherr@users.noreply.github.com>
2024-03-21 10:19:46 +0100

    initial upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup 
+  
+setup( 
+    name='cworkers', 
+    version='0.1', 
+    description='Simply simulation of workers who work or take vacation.', 
+    author='Curdin Lieberherr', 
+    author_email='curdin.lieberherr@bdo.ch', 
+    packages=['cworkers'], 
+    install_requires=[ 
+        'numpy', 
+        'pandas', 
+        'simpy',
+        'random'
+    ], 
+) 
